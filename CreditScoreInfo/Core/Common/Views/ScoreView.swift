bce802f28b8ef95a52ce78ce47f2c786923c86bf
Alexandre Freire <xandrefreire@gmail.com>
2019-01-30 10:27:50 +0000

    Add ScoreView

---
+++ CreditScoreInfo/Core/Common/Views/ScoreView.swift
@@ -0,0 +1,18 @@
+//
+//  ScoreView.swift
+//  Core
+//
+//  Created by Alexandre Freire on 30/01/2019.
+//  Copyright © 2019 Alexandre Freire. All rights reserved.
+//
+
+import UIKit
+
+class ScoreView: AFView {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var maxScoreValueLabel: UILabel!
+    
+
+}
